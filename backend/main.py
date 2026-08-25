36ad0b7643bd9ec1fc422d1ac9ae5bd47e15688d
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2026-08-25 13:38:40 +0300

    .

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://pdf-splitter-coral-eight.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
+    # استخدم "*" للسماح بأي دومين في مرحلة التجربة
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
