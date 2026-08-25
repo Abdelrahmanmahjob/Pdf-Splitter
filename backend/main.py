4901fc098019196a46774f009c657cd196fb7165
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2026-08-25 13:46:41 +0300

    .

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    # استخدم "*" للسماح بأي دومين في مرحلة التجربة
-    allow_origins=["*"],
-    # allow_credentials=True,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://pdf-splitter-coral-eight.vercel.app",
+    ],
+    # السماح بكل دومينات vercel الفرعية الخاصة بمشروعك بشكل ديناميكي
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
