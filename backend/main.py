e7eda798e404ea5a1966827b7cb08d551e323fcb
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2026-08-25 13:09:00 +0300

    Update CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://your-frontend.vercel.app",
+        "https://*.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
